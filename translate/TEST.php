aa1f8baf9851649297d12cdea9a94ad526b8181f
Yahe <hello@yahe.sh>
2018-06-03 09:00:35 +0200

    enhanced translate test

--- translate/TEST.php
+++ translate/TEST.php
@@ -15,15 +15,19 @@
   require_once(SYSTEM_PATH."defaults.php");
 
   require_once(SYSTEM_CORE_PATH."Base.class.php");
+  require_once(SYSTEM_CORE_PATH."Main.class.php");
   require_once(SYSTEM_CORE_PATH."Translate.class.php");
 
   var_dump(Translate::LANGUAGE());
 
-  Translate::LANGUAGE("en_US");
-  var_dump(Translate::LANGUAGE());
-
+  var_dump(Translate::register(__DIR__."/resources/platzhalter/", "platzhalter"));
   var_dump(Translate::register(__DIR__."/resources/test1/", "test1"));
   var_dump(Translate::register(__DIR__."/resources/test2/", "test2"));
+
+  var_dump(tfhtml("%s", "platzhalter", "[PLATZHÄLTER]"));
+
+  Translate::LANGUAGE("en_US");
+  var_dump(Translate::LANGUAGE());
 
   var_dump(t("das ist ein Test",         "test1"));
   var_dump(t("das ist ein zweiter Test", "test1"));
